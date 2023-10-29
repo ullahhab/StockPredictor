7ba75e635b640fb8d1293895aae6a8f36f05d657
ullahhab <hammadullahris@gmail.com>
2023-10-29 02:15:48 -0500

    stock pred list

--- stockPred.py
+++ stockPred.py
@@ -4,6 +4,7 @@ stocklst = ''
 #msft = yf.Ticker("msft")
 
 #print(msft.download(period="1mo"))
+data = ""
 
 def getSymbl():
     global stocklst
@@ -17,6 +18,7 @@ def getSymbl():
         
 
 def getTicker():
+    global data, stocklst
     getSymbl()
     """for symbl in stocklst:
         ticker = yf.Ticker(symbl)
@@ -24,6 +26,30 @@ def getTicker():
         print(test)
         print(ticker.history(period='1mo'))
     """
-    data = yf.download(stocklst[:len(stocklst)-2], period='1mo')
-    
-getTicker()
+    print(stocklst[:len(stocklst)-2])
+    stocklst = stocklst.split()
+    total = len(stocklst)
+    onePerc = int(total*0.01)
+    print(onePerc, total)
+    counter = 1
+    perc = 0
+    for stock in stocklst:
+        try:
+            if counter % onePerc ==0:
+                perc+=1
+                print(perc)
+            if '^' in stock:
+                stock = stock[:stock.index("^")]
+            data = yf.download(stock, period='1mo', progress=False)
+            counter+=1
+        except Exception as e:
+            #print(e)
+            continue
+
+def doAnalysis():
+    getTicker()
+    for stock in data:
+        print(stock)
+        print('\n\n ***************New Stock****************** \n')
+
+doAnalysis()
