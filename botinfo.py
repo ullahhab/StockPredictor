1f6733d1873545410bb17dfc1b2f07a0a499eb5a
ullahhab <hammadullahris@gmail.com>
2023-11-21 18:31:50 -0600

    bot continuation

--- botinfo.py
+++
@@ -1,9 +0,0 @@
-def seperate(value):
-    return value.split("=")[1].strip(" ")
-
-
-file = open('botinfo.txt', 'w')
-file.write("bought = True\nstockBought ="+str(0)+"\nbuyPrice =" +str(0)+"\nsellPrice ="+str(0)+"\nshares ="+str(0)+"\nmoney ="+str(0)+"\nvalue ="+"0"+'\n')
-file.close()
-
-
